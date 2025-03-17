a21187c20df0a65e5c00adbda4077a278340e140
Shalini <shaay208@mymail.unisa.edu.au>
2025-03-17 23:16:48 +1030

    first commit

---
+++ Practical2/Practical2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+class Pie {
+public:
+	virtual std::string description() const = 0;
+	virtual ~Pie() {}
+};
+
+class ApplePie : public Pie {
+public:
+	std::string description() const override {
+		return "Apple Pie";
+	}
+};
+
+class RaspberryPie : public Pie {
+public:
+	std::string description() const override {
+		return "Raspberry Pie";
+	}
+};
+
+int main() {
+	ApplePie p;
+	RaspberryPie r;
+
+	std::cout << p.description() << std::endl;
+	std::cout << r.description() << std::endl;
+
+	return 0;
+}
